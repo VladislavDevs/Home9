d15dc040ecc050360058405417801223ef319af6
VladislavDevs <supermars5327@gmail.com>
2024-12-17 20:20:57 +0700

    Домашнее задание

---
+++ CarNumber.py
@@ -0,0 +1,9 @@
+import re
+
+def validate_car_number(pattern : str, car_num : str):
+    print()
+
+
+def main():
+    car_num = input("Введите номер машины: ")
+    validate_car_number("", car_num)
---
+++ RemoveDuplicates.py
@@ -0,0 +1,9 @@
+import re
+
+def remove_duplicate(pattern : str, string_data : str):
+    print()
+
+
+def main():
+    string_data = input("Введите номер машины: ")
+    remove_duplicate("", string_data)
